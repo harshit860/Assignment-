8b01abc8126f72ea1f57d41e22d082e0713cbb04
harshit860 <harshitga@gmail.com>
2020-06-28 15:19:37 +0530

    2

--- 2/index.js
+++ 2/index.js
@@ -1,47 +1,28 @@
 console.log("connected")
 let flag = false
-let button_text = "Click me    [  Open  ]"
-
 
 let btn = document.getElementById('me')
-btn.textContent = button_text
-btn.onclick = function(){
+
+let main  = document.getElementById("main")
+btn.addEventListener('click', function(e){
+    
     flag = !flag
-    let main  = document.getElementById("main")
+    
     if(flag == true)
     {
         main.removeAttribute('class')
-        btn.textContent = "Click me    [  Close  ]"
+       
     }
     else
     {
         main.setAttribute('class','hide')
-        btn.textContent = "Click me    [  Open  ]"
+        
     }
    
-}
+})
 
-let sub1 = document.getElementById('sub1')
-let flag2  = false
+main.addEventListener('click',function (e){
 
-sub1.onclick = function () {
-        flag = !flag
-        let main2 = document.getElementById("main2")
-        if(flag == true)
-        {
-            main2.removeAttribute('class')
-            
-        }
-        else
-        {   
-                main2.setAttribute('class','hide')
-        }
-}
-
-let body = document.getElementsByTagName("body")
-console.log(body)
-btn.oncancel = function (){
-    let main  = document.getElementById("main")
+    btn.textContent = e.target.innerText
     main.setAttribute('class','hide')
-
-}
+})
